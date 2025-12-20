b6c53ababadf914a6998981b13082ec587e46f34
Ashish Sen <ashishsen2609@gmail.com>
2025-12-20 11:07:53 +0530

    chnaged base url

--- frontend/src/utils/Apiinstance.jsx
+++ frontend/src/utils/Apiinstance.jsx
@@ -1,7 +1,5 @@
 import axios from "axios";
-
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api/v1";
-
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://promo-site.onrender.com/api/v1";
 const api = axios.create({
   baseURL: BASE_URL,
 });
